b16a3ef99b90b241f4ef784e753594ab7b259eb2
Sayburn <s52skulloutlaw@gmail.com>
2022-07-25 22:00:32 +0800

    edited ex_02.py added age, gender, and position

--- ex_02_02/ex_02.py
+++ ex_02_02/ex_02.py
@@ -3,10 +3,18 @@
 nam=input('Who are you?')
 print('Hello', nam)
 
-age=input('How old are you?')
-
+age=input('How old are you? ')
+gender=input('What is your gender? ')
+position=input('What is your position? ')
 #Exercise: Write a program to prompt the user for hours and rate per hour to compute gross pay.
 hours=35
 rate=2.75
 pay=hours * rate
 print(pay)
+
+print(nam)
+print(age)
+print(gender)
+print(position)
+
+
