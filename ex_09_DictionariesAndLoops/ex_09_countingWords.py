436e7f02f92c7b893ced71eed142037b351f6fa8
Sayburn <s52skulloutlaw@gmail.com>
2022-08-15 12:23:35 +0800

    counting words using dictionaries, loops, conditionals

--- ex_09_DictionariesAndLoops/ex_09_countingWords.py
+++ ex_09_DictionariesAndLoops/ex_09_countingWords.py
@@ -1,1 +1,26 @@
-fname = input('Enter words : ')
+fname = input('Enter File : ')
+if len(fname) < 1 : fname = 'clown.txt'
+finam = open(fname)
+
+di = dict()
+for line in finam :
+	line = line.rstrip()
+	#print(line)
+	wds = line.split()
+	#print(wds)
+	for w in wds :
+		if w in di :
+			di[w] = di[w] + 1
+		else :
+			di[w] = 1
+			print('xxNEWxx')
+		print(w, di[w])
+#dick = dict()
+#for w in wds :
+#	print(w)
+
+#print('Type of wds :', type(wds))
+#print('Type of w W', type(w))
+#print(len(wds))
+#print(len(w))
+
