99325aca0495402faf0525bb69887ca6f15c4e24
Sayburn <s52skulloutlaw@gmail.com>
2022-12-18 06:16:33 +0800

    upload patients data

--- practic.py
+++ practic.py
@@ -1,14 +1,3 @@
-from itertools import count
-
-
-fhand = open('mbox-short.txt')
+fhand = open('sample.csv')
 for line in fhand:
-   line = line.rstrip()
-
-   count = 0
-   if line.startswith('From:'):
-      count = count + 1
-      line = line.split()
-      for z in line:
-         z = z.split('.')
-         print(z, count)
+   print(line)
